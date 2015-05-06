377c1485cf13e09bbe027889d32baa53d4a2332b
zacharing <zaw9998@saintjoe.edu>
2015-05-06 15:54:02 -0400

    Mammal.java done

--- FinalProjectZJA/src/edu/saintjoe/cs/zacharing/FinalProjectZJA/Mammals.java
+++ FinalProjectZJA/src/edu/saintjoe/cs/zacharing/FinalProjectZJA/Mammals.java
@@ -1,5 +1,39 @@
 package edu.saintjoe.cs.zacharing.FinalProjectZJA;
 
-public class Mammals {
+//Zachary Aring Final Project
+//May 6, 2015
 
+public class Mammals {
+	
+	private String type;
+	private int weight;
+	
+/*---------------------------------------------------*/
+	
+	public void FinalProject(String animalType, int animalWeight) {
+		type = animalType;
+		weight = animalWeight;
+	}
+	
+	public String toString() {
+		return "Mammal Type:" + type + "Weight:" + weight;
+	}
+	//Accessor
+	public String getType() {
+		return type;
+	}
+	//Accessor
+	public int getWeight() {
+		return weight;
+	}
+	//Mutator
+	public void setType(String newType) {
+		type = newType;
+	}
+	//Mutator
+	public void setWeight(int newWeight) {
+		weight = newWeight;
+	}
+	
 }
+
