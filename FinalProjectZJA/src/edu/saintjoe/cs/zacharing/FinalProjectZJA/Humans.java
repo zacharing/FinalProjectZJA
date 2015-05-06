f11d341ab55abdcc026dd1d624c531880abff127
zacharing <zaw9998@saintjoe.edu>
2015-05-06 16:38:10 -0400

    Humans updated

--- FinalProjectZJA/src/edu/saintjoe/cs/zacharing/FinalProjectZJA/Humans.java
+++ FinalProjectZJA/src/edu/saintjoe/cs/zacharing/FinalProjectZJA/Humans.java
@@ -1,5 +1,45 @@
 package edu.saintjoe.cs.zacharing.FinalProjectZJA;
 
 public class Humans {
+	
+	private String type;
+	private int weight;
+	private int iqscore;
+	
+/*---------------------------------------------------*/
+	
+	public void FinalProject(String humanType, int humanWeight, int humanIqscore) {
+		type = humanType;
+		weight = humanWeight;
+		iqscore = humanIqscore;
+	}
+	
+	public String toString() {
+		return "Human Type:" + type + "Weight:" + weight + "IQ Score:" + iqscore;
+	}
+	//Accessor
+	public String getType() {
+		return type;
+	}
+	//Accessor
+	public int getWeight() {
+		return weight;
+	}
+	
+	public int getIqscore() {
+		return iqscore;
+	}
+	//Mutator
+	public void setType(String newType) {
+		type = newType;
+	}
+	//Mutator
+	public void setWeight(int newWeight) {
+		weight = newWeight;
+	}
+	
+	public void setIqscore(int newIqscore) {
+		iqscore = newIqscore;
+	}
 
 }
